c7667d49bba87764c461012719b932fcf54f0391
aerdni99 <adi19@uakron.edu>
2025-02-21 16:12:12 -0500

    Add objective to index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-// EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
+/*
+  index.js
+
+  QA Wolf Take Home Job Application Assignment.
+  This script opens a chromium browser and goes to Hacker News.
+
+  My objective is to make sure that the 100 entries that pop up on the page are indeed sorted from newest to oldest.
+  I'll need to access the elements on the page and whatever attribute determines their age, then do some easy checks to see that each one is older than the last.
+*/
+
 const { chromium } = require("playwright");
 
 async function sortHackerNewsArticles() {
