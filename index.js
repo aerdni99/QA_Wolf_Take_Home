eb06d9489c47ceb7b1d7713e947446db2bb0b66c
aerdni99 <adi19@uakron.edu>
2025-02-21 22:21:22 -0500

    Progress on grabbing 100 rows

--- index.js
+++ index.js
@@ -11,6 +11,9 @@
   Each article has a title row and a subtext row. The title row has an index which appears to be serialized.
   I'll grab each article's ID and compare to make sure they appear in descending order.
 
+  In the case that ID's are not dependably created in ascending order, the solution is close to the same, 
+  but the element to analyze would be the <span class="age"> and its value would need to be converted to a date object for comparison.
+
   The page only displays 30 rows at a time. This makes sense why it is part of the application assignment haha 
   I'll have to use playwright to grab these rows and store them 30 at a time, then click more and do the next set until I get to 100.
 */
@@ -26,14 +29,23 @@ async function sortHackerNewsArticles() {
   // Go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
 
-  const articles = []
-  // Grab 30 rows
-  const thisSet = await page.locator(".athing").elementHandles();
-  articles.push(...thisSet);
+  const articles = [];
+
+  // Grab rows 1-30
+  articles.push(... await page.locator(".athing").elementHandles() );
+
+  // Press "More"
+  await page.locator(".morelink").click();
+
+  // Grab rows 31-60
+  articles.push(... await page.locator(".athing").elementHandles() );
 
   console.log(articles.length);
+  console.log(articles[0]);
+  console.log(articles[30]);
+  console.log(articles[0] == articles[30]);
 
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < articles.length; i++) {
     const article = articles[i];
   }
 
